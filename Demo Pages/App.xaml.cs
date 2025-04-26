ccfb04e9f1e480e1fe9a415cdd6d91164d36c8cf
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-04-25 21:08:39 -0500

    Agregar archivos de proyecto.

---
+++ Demo Pages/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_Pages
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
